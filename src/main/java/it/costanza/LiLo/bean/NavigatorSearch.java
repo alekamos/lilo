e2f766164667436b6f714d95606874001472652c
Alessandro Costanza <8929167+alekamos@users.noreply.github.com>
2015-01-08 12:54:30 +0100

    Sviluppi per il caricamento del navigator

---
+++ src/main/java/it/costanza/LiLo/bean/NavigatorSearch.java
@@ -0,0 +1,38 @@
+package it.costanza.LiLo.bean;
+
+import java.util.Date;
+
+public class NavigatorSearch {
+
+	
+	
+	private Date dateStart;
+	private Date dateEnd;
+	private long idUser;
+	
+	
+	
+	
+	public Date getDateStart() {
+		return dateStart;
+	}
+	public void setDateStart(Date dateStart) {
+		this.dateStart = dateStart;
+	}
+	public Date getDateEnd() {
+		return dateEnd;
+	}
+	public void setDateEnd(Date dateEnd) {
+		this.dateEnd = dateEnd;
+	}
+	public long getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(long idUser) {
+		this.idUser = idUser;
+	}
+	
+	
+	
+	
+}
