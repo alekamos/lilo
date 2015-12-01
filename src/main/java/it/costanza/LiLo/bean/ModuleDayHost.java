fda3c020fa1c4db4db67cb87d181edee29ccb05c
Alessandro Costanza <Alessandro Costanza@localhost>
2015-12-01 18:33:22 +0100

    Sviluppi su ricerca avanzata

--- src/main/java/it/costanza/LiLo/bean/ModuleDayHost.java
+++ src/main/java/it/costanza/LiLo/bean/ModuleDayHost.java
@@ -1,5 +1,8 @@
 package it.costanza.LiLo.bean;
 
+import it.costanza.LiLo.util.Utility;
+
+import java.util.ArrayList;
 import java.util.Date;
 
 public class ModuleDayHost{
@@ -43,6 +46,35 @@ public class ModuleDayHost{
 	public void setIdUser(long idUser) {
 		this.idUser = idUser;
 	}
+	
+
+	 @Override
+	    public String toString() {
+
+	    	ArrayList<String> chiavi = new ArrayList<String>();
+	    	ArrayList<String> valori = new ArrayList<String>();
+	    	
+	    	chiavi.add("dateDayHost");
+	    	chiavi.add("idModule");
+	    	chiavi.add("idModuleType");
+	    	chiavi.add("idModuleCluster");
+	    	chiavi.add("idUser");
+
+
+	    	
+	    	valori.add(dateDayHost+"");
+	    	valori.add(idModule+"");
+	    	valori.add(idModuleType+"");
+	    	valori.add(idModuleCluster+"");
+	    	valori.add(idUser+"");
+	    	
+	    	return Utility.toStringPerBean(chiavi, valori);
+	    	
+	    }
+	
+	
+	
+	
 	
 	
 }
